e0c8e658e425c7f010055bdc631e920d6d2f7beb
Armani <primearmani@gmail.com>
2023-10-05 12:22:42 +0100

    Python

---
+++ listpythonassignment/duplicatenumber.py
@@ -0,0 +1,9 @@
+numbers = [15, 20, 25, 20, 10, 5]
+multiplication = []
+for number in range [15, 20, 25, 20, 10, 5]:
+    if number is not numbers:
+        numbers.append(number)
+    else:
+        multiplication(number)
+
+print("The list of duplicate elements is :  " + str(number))
---
+++ listpythonassignment/smallestnumber.py
@@ -0,0 +1,5 @@
+numbers = [15, 20, 25, 20, 10, 5]
+numbers.sort()
+
+
+print(numbers[0])
